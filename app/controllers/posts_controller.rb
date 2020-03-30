c24393d7e9762a946976e09166bd6d9645b0a04d
Shunsuke Noguchi <bakutensanba89@yahoo.ne.jp>
2020-03-30 13:04:47 +0900

    added-main-function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   def index
-
+    @posts = Post.all.includes(:user).order('created_at ASC')
+  end
+  def show
+    @post = post.find_by(id: params[:id])
   end
   def new
     @post = Post.new
@@ -15,6 +19,23 @@ class PostsController < ApplicationController
       redirect_to root_path
       flash[:alert] = "追加に失敗しました"
     end
+  end
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+  def update
+    @post = Post.find_by(params[:id])
+    @post.update(params.require(:post).permit(:if, :then))
+    redirect_to  root_path
+  end
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    if @post.destroy
+      flash[:notice] = "削除されました"
+    else
+      flash[:alert] = "削除に失敗しました"
+    end
+    redirect_to root_path
   end
   private
   def post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :if, presence: true
+  validates :then, presence: true
 end
