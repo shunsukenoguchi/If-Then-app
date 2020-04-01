55bde9e107dedecc9a72e1648cf004a4b2679e1b
Shunsuke Noguchi <bakutensanba89@yahoo.ne.jp>
2020-04-01 18:31:17 +0900

    send grid setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method       = :smtp
-  config.action_mailer.default_url_options   = { host: 'herokuapp.com' }
+  config.action_mailer.default_url_options   = { host: 'https://if-then-app-mail-0401.herokuapp.com' }
   ActionMailer::Base.smtp_settings           = {
       address:              'smtp.sendgrid.net',
       port:                 '587',
